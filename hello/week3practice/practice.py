e04b3f33023d4b9bb47dcc81d71dd4d626701d68
brooke-hansen <ij4689bm@go.minnstate.edu>
2026-04-09 09:18:02 -0500

    update

---
+++ hello/week3practice/practice.py
@@ -0,0 +1,28 @@
+'''
+my_var = 
+if my_var % 2 == 1:
+    if my_var**3 != 27:
+        my_var = my_var +4 #Assignment 1
+    else:
+        my_var /= 1.5 #Assignment 2
+else:
+    if my_var <= 10:
+        my_var *= 2 #Assignment 3
+    else:
+        my_var -= 2 #Assignment 4
+print (my_var)
+'''
+'''
+#A3 light color
+light_color = input("Is the light red, green, or yellow: ")
+if light_color == 'red':
+    print("Stop")
+elif light_color == 'green':
+    print("Go")
+elif light_color == 'yellow':
+    print("Yield")
+'''
+#Q4 Ask a user to enter 3 numbers -> determine which is smallest
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter another number; "))
+num_3
